1192ecdec9dfc268e187912ee396f57cdaf6218b
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-28 15:26:11 +0300

    Create logged.py

---
+++ decorators_exercise/logged.py
@@ -0,0 +1,13 @@
+def logged(function):
+    def wrapper(*args):
+        return f"you called {function.__name__}{args}\nit returned {function(*args)}"
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+
+print(func(4, 4, 4))
