10d54f9fc3ef0ea668039f4c9b0879bda2f32b57
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-29 15:27:44 +0300

    Create team.py

---
+++ encapsulation_exercise/football_team_generator/project/team.py
@@ -0,0 +1,22 @@
+from test.project import Player
+
+
+class Team:
+    def __init__(self, name, rating):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []
+
+    def add_player(self, player: Player):
+        for pl in self.__players:
+            if pl.name == player.name:
+                return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name):
+        for pl in self.__players:
+            if pl.name == player_name:
+                self.__players.remove(pl)
+                return pl
+        return f"Player {player_name} not found"
