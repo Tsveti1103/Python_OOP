71b0193ce4533104b5edb29534c85ccd73787092
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-30 15:29:06 +0300

    Create profile.py

---
+++ encapsulation_lab/profile.py
@@ -0,0 +1,32 @@
+class Profile:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if 5 > len(value) or len(value) > 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        is_long = len(value) >= 8
+        upper_case = any([True for x in value if x.isupper()])
+        is_digit = any([True for x in value if x.isdigit()])
+        if is_long and upper_case and is_digit:
+            self.__password = value
+            return
+        raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
