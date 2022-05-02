6bc727e566b063d42c24ec2e556710814156bc14
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-02 15:30:11 +0300

    Create shop.py

---
+++ static_and_class_methods_lab/shop.py
@@ -0,0 +1,27 @@
+class Shop:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type):
+        return cls(name, type, 10)
+
+    def add_item(self, item_name):
+        if self.capacity <= sum(self.items.values()):
+            return "Not enough capacity in the shop"
+        if item_name not in self.items:
+            self.items[item_name] = 0
+        self.items[item_name] += 1
+        return f"{item_name} added to the shop"
+
+    def remove_item(self, item_name, amount):
+        if item_name not in self.items or self.items[item_name] < amount:
+            return f"Cannot remove {amount} {item_name}"
+        self.items[item_name] -= amount
+        return f"{amount} {item_name} removed from the shop"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
