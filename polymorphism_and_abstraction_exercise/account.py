e8a047ea4afcb8466ed42dea100856e50d71afc6
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-05 15:33:02 +0300

    Create account.py

---
+++ polymorphism_and_abstraction_exercise/account.py
@@ -0,0 +1,82 @@
+class Account:
+    def __init__(self, owner, amount=0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def add_transaction(self, amount):
+        if isinstance(amount, int):
+            self._transactions.append(amount)
+        else:
+            raise ValueError("please use int for amount")
+
+    @property
+    def balance(self):
+        return sum(self._transactions) + self.amount
+
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        new_balance = account.balance + amount_to_add
+        if new_balance < 0:
+            raise ValueError("sorry cannot go in debt!")
+        else:
+            account.add_transaction(amount_to_add)
+            return f"New balance: {account.balance}"
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, idx):
+        return self._transactions[idx]
+
+    def __reversed__(self):
+        return self._transactions[::-1]
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __add__(self, other):
+        result = Account(f"{self.owner}&{other.owner}", self.amount + other.amount)
+        for tx in self:
+            result.add_transaction(tx)
+        for tx in other:
+            result.add_transaction(tx)
+        return result
+
+
+acc = Account('bob', 10)
+acc2 = Account('john')
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
