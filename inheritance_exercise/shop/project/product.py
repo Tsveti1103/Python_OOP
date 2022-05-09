dc0f3a7cc5cb102d642f6e7561cdeaed09267a9d
Tsveti1103 <cvstoqnova@gmail.com>
2022-05-09 15:35:40 +0300

    Create product.py

---
+++ inheritance_exercise/shop/project/product.py
@@ -0,0 +1,14 @@
+class Product:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    def decrease(self, quantity):
+        if self.quantity - quantity >= 0:
+            self.quantity -= quantity
+
+    def increase(self, quantity):
+        self.quantity += quantity
+
+    def __repr__(self):
+        return self.name
